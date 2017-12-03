c1c3588fd9b5f2fdf2c53cd14c2091d031ba3e4c
Ron Kurr <kurr@kurron.org>
2017-12-03 16:22:29 -0500

    Upgrade to current release. Use Docker for testing.

--- tests/run-test.sh
+++ tests/run-test.sh
@@ -1,3 +1,32 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-ansible-playbook --verbose --inventory-file inventory --user vagrant --ask-pass --become-user root --ask-become-pass test.yml
+#ansible-playbook --verbose --inventory-file inventory --user vagrant --ask-pass --become-user root --ask-become-pass test.yml
+
+rm -rf /tmp/roles
+mkdir /tmp/roles
+cp -R ~/GitHub/ansible-role-base /tmp/roles/kurron.base
+cp -R ~/GitHub/ansible-role-jdk /tmp/roles/kurron.jdk
+cp -R ~/GitHub/ansible-role-ruby-developer /tmp/roles/kurron.ruby-developer
+
+packer validate packer.json
+packer build --force packer.json
+
+
+# Need to give the container access to your windowing system
+export DISPLAY=:0
+xhost +
+
+CMD="docker run --env DISPLAY=unix${DISPLAY} \
+                --env HOME=${HOME_DIR} \
+                --env RUBYMINE_JDK=/usr/lib/jvm/azul-jdk-8 \
+                --env JDK_HOME=/usr/lib/jvm/azul-jdk-8 \
+                --env JAVA_HOME=/usr/lib/jvm/azul-jdk-8 \
+                --net host \
+                --interactive \
+                --tty \
+                --rm \
+                --volume /tmp/.X11-unix:/tmp/.X11-unix \
+                --workdir /root/Software \
+                kurron/ansible-role-ruby-developer:latest bash"
+echo $CMD
+$CMD
